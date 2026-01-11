68e0d679cbb0e7ed726411c45088f5c069c78e3e
abrazh <ivanabrazhevich@yandex.ru>
2026-01-11 17:43:22 +0300

    second commit

---
+++ db-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root', 
+    database: 'airport', 
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
